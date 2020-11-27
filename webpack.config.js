4e2990274f6af8a0f131bea00da15d022ed852e0
yinhaiying <yinhaiying@58.com>
2020-11-27 12:15:25 +0800

    设置库的导出格式umd

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 
+const path = require("path");
 module.exports = {
     entry:{
         index:"./lib/index.tsx"
+    },
+    output:{
+      path:path.resolve(__dirname,"dist/lib"),
+      library:"sea-ui",
+      libraryTarget:"umd"//  库最终导出的模块格式。umd是amd和cmd的兼容处理
     },
     module:{
         rules:[
