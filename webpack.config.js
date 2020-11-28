36471951e57a8749c8bdba5a1cbe34c9d18751f7
yinhaiying <18840842571@163.com>
2020-11-28 08:22:29 +0800

    区分生产环境和开发环境并在package.json中配置对应的webppack.config.json

--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,24 @@ module.exports = {
             }
         ]
     },
-    plugins:[
-        new HtmlWebpackPlugin({
-            title:"sea-ui",
-            template:"index.html"
-        })
-    ]
+    // plugins:[
+    //     new HtmlWebpackPlugin({
+    //         title:"sea-ui",
+    //         template:"index.html"
+    //     })
+    // ],
+    // externals:{
+    //   "react":{
+    //     "commonjs":"react",
+    //     "commonjs2":"react",
+    //     "amd":"react",
+    //     "root":"React"
+    //   },
+    //   "react-dom":{
+    //     "commonjs":"react-dom",
+    //     "commonjs2":"react-dom",
+    //     "amd":"react-dom",
+    //     "root":"ReactDOM"
+    //   },
+    // }
 }
